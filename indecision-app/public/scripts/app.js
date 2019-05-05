01e367e4b0dfd108f88c6d4744cbdff5bc538501
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-05 23:19:37 +0530

    Counter Example Added

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -53,50 +53,5 @@ var template = React.createElement(
     )
 );
 
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-
-var resetApp = function resetApp() {
-    count = 0;
-    renderCounterApp();
-};
 var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Count : ',
-            count
-        ),
-        React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
-        ),
-        React.createElement(
-            'button',
-            { onClick: resetApp },
-            'Reset'
-        )
-    );
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -24,35 +24,5 @@ var template = (<div>
     </div>
     );
 
-let count = 0;
-const addOne = () => {
-    count ++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count --;
-    renderCounterApp();
-};
-
-const resetApp = () => {
-    count = 0;
-    renderCounterApp();
-};
 const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-        <h1>Count : {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={resetApp}>Reset</button>
-        </div>
-    );
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
-
-
+ReactDOM.render(template, appRoot);
