3f0cb708a4267696926395ba543f8ec59db4ad85
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-19 17:05:09 +0530

    Adding Default Props

--- StephenGrider/seasons/src/index.js
+++ StephenGrider/seasons/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-    }
+    state = { lat: null, errorMessage: ''};
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
@@ -22,9 +18,9 @@ class App extends React.Component{
             }
 
             if(!this.state.errorMessage && this.state.lat) {
-                return <div>Latitude: {this.state.lat}</div>
+                return <SeasonDisplay lat={this.state.lat} />
             }
-             return <div>Loading......</div>
+             return <Spinner message="Please accept the location request"/>
     }
 }
 
