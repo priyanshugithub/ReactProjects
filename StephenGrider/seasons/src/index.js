14b14a93e4f8fe5345d98f1bf3b92a350f5a821b
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-19 12:05:17 +0530

    Refractoring Data Loading to lifecycle methods

--- StephenGrider/seasons/src/index.js
+++ StephenGrider/seasons/src/index.js
@@ -8,14 +8,12 @@ class App extends React.Component{
             lat: null,
             errorMessage: ''
         };
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //we called setState to update our state object!!!!
-                this.setState({ lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errorMessage: err.message })
         );
     }
     render() {
