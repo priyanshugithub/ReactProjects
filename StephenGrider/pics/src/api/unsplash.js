27a16257c85088287e9d7aa00ee20d9feed1bbd2
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-22 18:03:25 +0530

    Updating API Key

--- StephenGrider/pics/src/api/unsplash.js
+++ StephenGrider/pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID deefdf01510843b34d3446ea627bbae2d58f7e68d8c66d8500e40b47e71db93c'
+        Authorization: 'Client-ID YOUR-API-KEY'
     }
 });
