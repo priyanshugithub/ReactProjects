c517aca74df92a6526c5cf6e8fcb31656eade23a
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-24 23:41:07 +0530

    Final Integrating React with Redux

--- StephenGrider/songs/src/components/SongDetail.js
+++ StephenGrider/songs/src/components/SongDetail.js
@@ -5,7 +5,16 @@ const SongDetail = ({ song }) => {
     if(!song){
         return <div>Select a song!</div>;
     }
-    return <div>{song.title}</div>;
+    return(
+        <div>
+            <h3>Details for:</h3>
+            <p>
+            Title: {song.title}
+            <br />
+            Duration: {song.duration}
+            </p>
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
