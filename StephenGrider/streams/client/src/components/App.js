46f24a226140dc6c625e9ce7e82eb3885f9257c3
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-28 22:40:09 +0530

    Initial Setup

--- StephenGrider/streams/client/src/components/App.js
+++ StephenGrider/streams/client/src/components/App.js
@@ -1,32 +1,7 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import React from 'react';
 
-import Header from './Header';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import SurveyNew from './surveys/SurveyNew';
+const App = () => {
+    return <div>App</div>;
+};
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchUser();
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
-
-export default connect(null, actions)(App);
+export default App;
