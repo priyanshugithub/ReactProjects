1d7256e98947b205e01192a12c21d186d5c3e125
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-07-03 21:04:39 +0530

    Initial Setup

---
+++ react-andrei/monsters-rolodex/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name:'Frankenstein'
+        },
+        {
+          name:'Dracula'
+        },
+        {
+          name:'Zombie'
+        }
+      ]
+    };
+  }
+  render() {
+  return (
+    <div className="App">
+      {
+        this.state.monsters.map(monster => <h1> { monster.name } </h1>)
+      }
+    </div>
+  );
+}
+}
+
+export default App;
