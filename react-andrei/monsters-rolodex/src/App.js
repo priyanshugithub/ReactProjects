7b2d9b40ff5db6ea5fe11343c2e1b99f135bdee1
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-07-11 12:29:19 +0530

    Creating Card Lists

--- react-andrei/monsters-rolodex/src/App.js
+++ react-andrei/monsters-rolodex/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import CardList from './components/card-list/card-list';
 import './App.css';
+
 
 class App extends Component {
   constructor() {
@@ -15,12 +17,13 @@ class App extends Component {
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
   }
+  
   render() {
   return (
     <div className="App">
-      {
-        this.state.monsters.map(monster => <h1> { monster.name } </h1>)
-      }
+    <CardList monsters={this.state.monsters}>
+    
+    </CardList>
     </div>
   );
 }
